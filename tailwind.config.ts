9ea3c4969901a05a1274ba49a9389b1088ff4f6b
Delfin F <DelfinoDevShark@gmail.com>
2024-11-29 00:29:56 +0100

    custom shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,10 @@ export default {
 		extend: {
 			fontFamily: {
 				sans: ["Figtree", "sans-serif"] // Add Figtree as the primary font
+			},
+			boxShadow: {
+				"custom-shadow":
+					"0 15px 25px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.15)"
 			}
 		}
 	},
